e7b486dca491a2f3f7cee2af0468891e91436ee0
Brenden A Domingo <63617812+BrendenDomingo@users.noreply.github.com>
2023-05-02 21:10:01 -0400

    Coins working (#33)
    * Coins working
    
    * remove mat

--- Assets/Scripts/Enemy/Enemy2D.cs
+++ Assets/Scripts/Enemy/Enemy2D.cs
@@ -12,6 +12,11 @@ public class Enemy2D : MonoBehaviour
     public GameObject player;
     public GameObject itemDrop;
 
+    public int coinsToDrop; // the number of coins to drop upon death
+    public float coinSpeed; // the speed at which the coins will be shot out
+    public GameObject coinPrefab; // the coin prefab to be instantiated
+    public LayerMask enemyLayer; // the layer(s) to collide with the environment
+
     public bool isAttacking;
 
     public virtual void ReceiveDamage ( float damage )
@@ -41,6 +46,29 @@ public class Enemy2D : MonoBehaviour
         if ( itemDrop != null )
         {
             Instantiate( itemDrop, transform.position, transform.rotation );
+        }
+
+        // Create and shoot out coins
+        for (int i = 0; i < coinsToDrop; i++)
+        {
+            Vector2 randomDirection = new Vector2 ( Random.Range ( -1f, 1f ), Random.Range ( 0f, 1f ) ).normalized;
+            float randomSpeed = Random.Range ( coinSpeed * 0.5f, coinSpeed * 1.5f );
+            GameObject coin = Instantiate ( coinPrefab, transform.position, Quaternion.identity );
+
+            // Set the layer of the coin to "Coins" to exclude it from colliding with the "Enemy" layer
+            coin.layer = LayerMask.NameToLayer ( "Coins" );
+
+            Rigidbody2D coinRigidbody = coin.GetComponent<Rigidbody2D> ( );
+            coinRigidbody.AddForce ( randomDirection * randomSpeed, ForceMode2D.Impulse );
+
+            // Ignore collisions with the enemy's collider
+            Physics2D.IgnoreCollision ( coin.GetComponent<Collider2D> ( ), GetComponent<Collider2D> ( ) );
+
+            // Ignore collisions with other coins
+            Physics2D.IgnoreLayerCollision ( coin.layer, coin.layer );
+
+            // Ignore collision between the "Enemy" layer and the "Coins" layer
+            Physics2D.IgnoreLayerCollision ( LayerMask.NameToLayer ( "enemy" ), LayerMask.NameToLayer ( "Coins" ), true );
         }
 
         // Record death 
